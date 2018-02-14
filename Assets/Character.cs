4b8f6c90d1a8235a6316ebae96c5cc7ad4a14177
BuildTools <unconfigured@null.spigotmc.org>
2018-02-14 20:40:01 +0200

    Added character movement

---
+++ Assets/Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character : MonoBehaviour {
+
+    Rigidbody2D rigidbody2d;
+
+    [SerializeField] float characterMovementSpeed = 1000f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2d.AddRelativeForce(Vector2.left * characterMovementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2d.AddRelativeForce(Vector2.right * characterMovementSpeed * Time.deltaTime);
+        }
+    }
+
+
+}
