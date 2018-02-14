1349748a8677148bbd6536250e4a0946290a4442
idanro <idan.rooze@gmail.com>
2018-02-15 01:35:37 +0200

    Fixed most jump issues + revamped animator

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -15,82 +15,101 @@ public class Character : MonoBehaviour {
     [SerializeField] bool grounded = false;
 
     public Transform groundCheck;
-    float groundRadius = 0.2f;
+    float groundRadius = 0.05f;
     public LayerMask whatIsGround;
     Vector3 velocity;
 
+    bool facingRight = true;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         grounded = false;
         anim = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
 
     {
+        anim.SetFloat("vSpeed", velocity.y);
+        anim.SetFloat("xSpeed", Math.Abs(velocity.x));
+        print(velocity.x);
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-        print(grounded);
         velocity = rigidbody2d.velocity;
         ProcessPlayerInput();
         ControlAnimations();
+        anim.SetBool("Grounded", grounded);
     }
 
     private void ProcessPlayerInput()
+    {
+        CheckMoveLeft();
+        CheckMoveRight();
+        CheckJump();
+    }
+
+    private void CheckMoveLeft()
     {
         if (Input.GetKey(KeyCode.A))
         {
             rigidbody2d.AddRelativeForce(Vector2.left * characterMovementSpeed * Time.deltaTime);
         }
+    }
+
+    private void CheckMoveRight()
+    {
         if (Input.GetKey(KeyCode.D))
         {
             rigidbody2d.AddRelativeForce(Vector2.right * characterMovementSpeed * Time.deltaTime);
         }
+    }
+
+    private void CheckJump()
+    {
         if (Input.GetKey(KeyCode.Space) && grounded)
         {
-            rigidbody2d.AddForce(Vector2.up * characterJumpHeight);
-            InitiateJumpingAnimation();
+            Invoke("Jump", 1);
         }
+    }
+
+    private void Jump()
+    {
+            rigidbody2d.AddForce(Vector2.up * characterJumpHeight);
     }
 
     private void ControlAnimations()
     {
-        if (grounded)
-            if (velocity.x == 0)
-            {
-                InitiateIdleAnimation();
-            }
-            else
-            {
-                InitiateWalkingAnimation();
-            }
-        else
+        TurnLeftRight();
+        //TriggerAnimations();
+    }
+
+    private void TurnLeftRight()
+    {
+        if (velocity.x > 0.1 && !facingRight)
         {
-            InitiateInAirAnimation();
-            print("InAirAnimationActivated");
+            Flip();
+        }
+        else if (velocity.x < -0.1 && facingRight)
+        {
+            Flip();
         }
     }
 
-    private void InitiateIdleAnimation()
+    void Flip()
     {
-        anim.SetInteger("State", 0);
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
-    private void InitiateWalkingAnimation()
-    {
-        anim.SetInteger("State", 1);
-    }
+    private void InitiateIdleAnimation() { anim.SetInteger("State", 0); }
 
-    private void InitiateJumpingAnimation()
-    {
-        anim.SetInteger("State", 2);
-    }
+    private void InitiateWalkingAnimation() { anim.SetInteger("State", 1); }
 
-    private void InitiateInAirAnimation()
-    {
-        anim.SetInteger("State", 3);
-    }
+    private void InitiateJumpingAnimation() { anim.SetInteger("State", 2); }
 
+    private void InitiateInAirAnimation() { anim.SetInteger("State", 3); }
 }
